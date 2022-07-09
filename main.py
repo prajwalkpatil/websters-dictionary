42ece00a760d25a9ec8ada1127302bcd0bfc2890
Prajwal Patil <prajwalpatilk@gmail.com>
2022-07-09 18:24:35 +0530

    Add a main function

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import pandas as pd
 import pickle
 
 dictionary = {}
+all_main_words = []
 sentences_merged = []
 
-def isMainWord(a):
+def is_main_word(a):
     flag = True
     if a == '\n':
         return False
@@ -16,13 +17,13 @@ def isMainWord(a):
     return flag
 
 
-def mergeSentences(l):
+def merge_sentences(l):
     i = 0
     temp_string = ""
     while i < len(l):
-        if isMainWord(l[i]):
+        if is_main_word(l[i]):
             temp_string = l[i].replace("\n","")
-            print(f">>> {temp_string}")
+            # print(f">>> {temp_string}")
             sentences_merged.append(temp_string)
             temp_string = ""
             i = i + 1
@@ -31,36 +32,36 @@ def mergeSentences(l):
                 temp_string = temp_string + l[i].replace("\n"," ")
                 i = i + 1
             sentences_merged.append(temp_string)
-            print(f">> {temp_string}")
+            # print(f">> {temp_string}")
             temp_string = ""
         else:
             i = i + 1
     with open('files/mergedSentences.pkl', 'wb') as f:
         pickle.dump(sentences_merged, f)
 
-'''
-#To Retrieve the list: 
+def read_main_words_file():
+    with open('files/mainWords.pkl', 'rb') as f:
+        all_main_words = pickle.load(f)
+    return all_main_words
+
+def read_merged_sentences_file():
     with open('files/mergedSentences.pkl', 'rb') as f:
         sentences_merged = pickle.load(f)
-'''
-'''
-def open_read():
-    with open('files/mainSentences.pkl', 'rb') as f:
-        all_main_words = pickle.load(f)
-    print(all_main_words)
-'''
+    return sentences_merged
+
 
 def read_file():
     fp = open("dictionary.txt","r")
     all_strings = fp.readlines()
-    all_main_words = []
     for i in all_strings:
-        if isMainWord(i):
+        if is_main_word(i):
             all_main_words.append(i.replace("\n",""))
     with open('files/mainWords.pkl', 'wb') as f:
         pickle.dump(all_main_words, f)
-    mergeSentences(all_strings)
+    merge_sentences(all_strings)
 
-
-# read_file()
-read_file()
+def main():
+    all_main_words = read_main_words_file()
+    sentences_merged = read_merged_sentences_file()
+    
+main()
