74faabd9ac1e41f6611cd877561eda32e9a7522e
Prajwal Patil <prajwalpatilk@gmail.com>
2022-07-17 14:40:46 +0530

    Add comments

--- gui.py
+++ gui.py
@@ -1,30 +1,46 @@
+# ---------------------------------------------------------------------------- #
+#               Graphical User Interface for Webster's dictionary              #
+# ---------------------------------------------------------------------------- #
+
 import tkinter as tk
 import ctypes
+# ---------------------------- Import from main.py --------------------------- #
 from main import *
 
+
+# ----------------------------- Global variables ----------------------------- #
 temp_dict = []
+#2D list to store the word-recommendations 
 temp_rec = []
-window_count = 0 
+#Global variable to count the number of word-recommendation windows created 
+window_count = 0  
 root = tk.Tk()
 
+# ------------ Function to create windows for word-recommendations ----------- #
 def create_recommendations_window(word):
     global window_count
+    #Set a window ID -> starting from 0
     window_id = window_count
     window_count += 1
+    #Creates another window
     window_recommendations = tk.Toplevel(root)
     window_recommendations.geometry("1150x600")
     window_recommendations.title(f"Webster's Dictionary - {word.lower().capitalize()} recommendations")
     word_recommendations_window_label = tk.Label(window_recommendations, text = f"Recommendations for \"{word.lower().capitalize()}\"")
     listbox_recommendations = tk.Listbox(window_recommendations,height= 500, width = 50)
     listbox_recommendations['font'] = "Georgia 15"
+    #Creates recommendations from the given word
     recommendations = create_recommendations(word.upper())
+    #Appends an empty list to the temp_rec list
     temp_rec.append([])
     l = 0
     for i in recommendations:
         listbox_recommendations.insert(l,i.lower().capitalize())
         l += 1;
+        #Insert in the temp_rec list
         temp_rec[window_id].append(i)
 
+    # Function to be triggered when an object is selected from the recommendation list box
     def items_selected(event):
         user_selection = listbox_recommendations.curselection()
         if len(user_selection) != 0:
@@ -35,22 +51,28 @@ def create_recommendations_window(word):
     word_recommendations_window_label.config(font =("Georgia", 22))
     word_recommendations_window_label.pack(pady=(10,10))
     listbox_recommendations.pack(pady=(20,2))
+    #Binding the function items_selected to the event when a selection is made in the listbox
     listbox_recommendations.bind('<<ListboxSelect>>', items_selected) 
     window_recommendations.mainloop()
 
+# ------- Function to create window for word meanings and descriptions ------- #
 def create_window(word):
+    # To print the selected word and meanings in the console
     print(dictionary[word.upper()])
+    #Creates a seperate window for the meanings of each word
     window = tk.Toplevel(root)
     window.title(f"Webster's Dictionary - {word.lower().capitalize()}")
     window.geometry("1150x600")
     word_window_label = tk.Label(window, text = word.lower().capitalize())
     word_window_label.config(font =("Georgia", 30))
     word_window_label.pack(pady=(10,10))
+    #Create a label that acts also as a hyper-link to view recommendations for each word
     recommendations_label = tk.Label(window, text = f"See references/recommendations for - {word.lower().capitalize()}", foreground= "blue")
     recommendations_label.config(font =("Georgia", 12,"italic"))
     recommendations_label.pack(pady=(10,10))
+    #Binding the function create_recommendations_window to the event when a selection is made in the listbox
     recommendations_label.bind("<Button-1>", lambda e:create_recommendations_window(word))
-
+    #Insert meanings and etymology for each word as a text label
     for i in dictionary[word.upper()]:
         word_et_label = tk.Label(window, text = i['et'],wraplength= 1000)
         word_et_label.config(font =("Georgia", 12 , "italic"))
@@ -65,10 +87,10 @@ def create_window(word):
 def display(): 
     root.title("Webster's Dictionary")
     root.geometry("1080x1080")
+    #Increase the DPI of the display window
     ctypes.windll.shcore.SetProcessDpiAwareness(1)  
-
     root.tk.call('tk', 'scaling', 2)
-
+    #Insert text lables 
     title_label = tk.Label(root, text = "Webster's Dictionary - Search Engine")
     input_description = tk.Label(root, text = "Enter your query here ")
     search_recommendations_label = tk.Label(root, text = "Search Recommendations")
@@ -77,6 +99,7 @@ def display():
     input_description.config(font =("Georgia", 13,"bold"))
     search_recommendations_label.config(font =("Georgia", 15,"bold"))
 
+    #Insert search recommendations when user searches for a word
     def label_value():
         j = 0
         listbox.delete(0,tk.END)
@@ -89,8 +112,8 @@ def display():
             for i in temp_dict:
                 listbox.insert(j, i[0].lower().capitalize())
                 j += 1
-                # print(i[0].lower().capitalize())
 
+    #Function to create a window for each word with its desciption and etymology
     def items_selected(event):
         user_selection = listbox.curselection()
         if len(user_selection) != 0:
@@ -102,9 +125,9 @@ def display():
     entry = tk.Entry(root, textvariable=textvar)
     entry['font'] = "Georgia 20"
     entry.focus()
-    
+    #Binding the function label_value to the event when user presses enter
     entry.bind("<Return>", lambda x: label_value())
-    
+
     listbox = tk.Listbox(root,height= 500, width = 50)
     listbox['font'] = "Georgia 15"
     title_label.pack(pady=(20,2))
@@ -112,6 +135,7 @@ def display():
     entry.pack(pady=(20,2))
     search_recommendations_label.pack(pady=(30,5))
     listbox.pack(pady=(20,2))
+    #Binding the function items_selected to the event when a selection is made in the listbox
     listbox.bind('<<ListboxSelect>>', items_selected)
 
     tk.mainloop()
--- main.py
+++ main.py
@@ -1,14 +1,32 @@
+# ---------------------------------------------------------------------------- #
+#                     Webster's Dictionary - main functions                    #
+# ---------------------------------------------------------------------------- #
+
+# ------------------------------ Import packages ----------------------------- #
 import csv
 import pandas as pd
 import pickle
 import json
 import string 
 
+# ----------------------------- Global variables ----------------------------- #
+#Dictionary containing all the words with their meanings
 dictionary = {}
+#A list containing all the main words
 all_main_words = []
+#A list containing all the single sentences merged by removing the newline chars  
 sentences_merged = []
+#A set containing all the word-recommendations
 recommendations = set()
 
+# ---------------------------------------------------------------------------- #
+# -------------- Function to determine if a word is a main word -------------- #
+# ---------------------------------------------------------------------------- #
+#      Conditons for a word to be a main word as per the given text file,      #
+#      - It should be on a single line.                                        #
+#      - It should contain only uppercase letters.                             #
+#      - It can contain spaces and semicolons.                                 #
+# ---------------------------------------------------------------------------- #
 def is_main_word(a):
     a = a.replace("\n","")
     flag = True
@@ -22,84 +40,92 @@ def is_main_word(a):
             break
     return flag
 
-
+# - Function to merge all the single sentences by removing the newline chars - #
 def merge_sentences(l):
     i = 0
     temp_string = ""
     while i < len(l):
+        #If it's a main word just append the word to the list 
         if is_main_word(l[i]):
             temp_string = l[i].replace("\n","")
-            # print(f">>> {temp_string}")
             sentences_merged.append(temp_string)
             temp_string = ""
             i = i + 1
+        #Append the sentences untill a seperate newline or mainword is encountered
         elif l[i] != '\n':
             while i < len(l) and l[i] != '\n':
                 temp_string = temp_string + l[i].replace("\n"," ")
                 i = i + 1
             sentences_merged.append(temp_string)
-            # print(f">> {temp_string}")
             temp_string = ""
         else:
             i = i + 1
+    #Dump the merged sentences to a pickle file
     with open('files/mergedSentences.pkl', 'wb') as f:
         pickle.dump(sentences_merged, f)
 
+# ---------- Function to read the pickle file containing main words ---------- #
 def read_main_words_file():
     with open('files/mainWords.pkl', 'rb') as f:
         all_main_words = pickle.load(f)
     return all_main_words
 
+# -------- Function to read a pickle file containing merged sentences -------- #
 def read_merged_sentences_file():
     with open('files/mergedSentences.pkl', 'rb') as f:
         sentences_merged = pickle.load(f)
     return sentences_merged
 
-
+# -- Function to read the given dictionary.txt file and find the main words -- #
 def read_file():
     fp = open("dictionary.txt","r")
     all_strings = fp.readlines()
     for i in all_strings:
+        #If its a main word then append it to the all_main_words
         if is_main_word(i):
             all_main_words.append(i.replace("\n",""))
+    #Dump the main word to a pickle file
     with open('files/mainWords.pkl', 'wb') as f:
         pickle.dump(all_main_words, f)
+    #Call merge_sentences to merge the sentences with newline
     merge_sentences(all_strings)
 
+# ----------- Function to make dictionary from the merged sentences ---------- #
 def make_dictionary(sentences_merged):
     i = 0
     while i < len(sentences_merged):
         if is_main_word(sentences_merged[i]):
             key_word = sentences_merged[i]
             i = i + 1 
+            #If the key_word is not in the dictionary, update the dictionary
             if key_word not in dictionary: 
                 dictionary.update({key_word : []})
+            #Create a dictionary inside an array for each meaning of a word
             dictionary[key_word].append({'et' :"", 'meanings':[]})
             last_index = len(dictionary[key_word]) - 1
+            #Insert etymology for a key_word
             dictionary[key_word][last_index]['et'] = sentences_merged[i]
             i = i + 1
             while i < len(sentences_merged) and not is_main_word(sentences_merged[i]):
                 if sentences_merged[i] != '\n':
+                    #Append meanings to the meaning array for a keyword
                     dictionary[key_word][last_index]['meanings'].append(sentences_merged[i])
                 i = i + 1
-            '''
-            print(f">> {key_word}")
-            print(dictionary[key_word]) 
-            print("\n")
-            '''
         i = i + 1
+    #Dump the dictionary to a JSON file
     jsonString = json.dumps(dictionary)
     jsonFile = open("files/Dictionary.json", "w")
     jsonFile.write(jsonString)
     jsonFile.close()
     return dictionary
 
+# ---------------- Function to read dictonary from a JSON file --------------- #
 def read_dictionary():
     with open("files/Dictionary.json") as file:
         dictionary = json.load(file)
     return dictionary
 
-
+# ------------------- Class denoting each node of a Trie ------------------- #
 class TrieNode:
     def __init__(self, char):
         self.char = char
@@ -107,11 +133,13 @@ class TrieNode:
         self.counter = 0
         self.children = {}
 
-
+# ----------- Trie class with the methods to perform the operations ---------- #
 class Trie(object):
+    #Init 
     def __init__(self):
         self.root = TrieNode("")
     
+    #Method to insert a word
     def insert(self, word):
         node = self.root
         for char in word:
@@ -123,14 +151,13 @@ class Trie(object):
                 node = new_node
         node.is_end = True
         node.counter += 1
-        
+    #Method to perform Depth First Search
     def dfs(self, node, prefix):
         if node.is_end:
             self.output.append((prefix + node.char, node.counter))
-        
         for child in node.children.values():
             self.dfs(child, prefix + node.char)
-        
+    #Method to query a keyword in the Trie and returning the query results   
     def query(self, x):
         self.output = []
         node = self.root
@@ -142,33 +169,46 @@ class Trie(object):
         self.dfs(node, x[:-1])
         return sorted(self.output, key=lambda x: x[1], reverse=True)
 
+# ------------------ Creating a trie called dictionary_trie ------------------ #
 dictionary_trie = Trie()   
 
+# ----------- Function to insert the dictionary keys into the Trie ----------- #
 def insert_main_words():
     for i in dictionary.keys():
         dictionary_trie.insert(i)
     return dictionary_trie
 
+# --- Function to return a set containing recommendations for a given word --- #
 def create_recommendations(word):
     word = word.upper()
     rec = set()
+    #Recommendations should only be from the dictionary
     if word not in dictionary:
         return rec
+    #Read each string from the meaning of the given word
     for me in dictionary[word]:
         for m in me['meanings']:
+            #Remove punctuation marks from the string
             temp_string = m.translate(str.maketrans('', '', string.punctuation))
+            #Remove digits from the string
             temp_string = temp_string.translate(str.maketrans('', '', string.digits))
+            #Seperate each words from the strings
             seperate_words = temp_string.split()
             for sep_word in seperate_words:
                 sep_word = sep_word.upper()
+                #If seperated word is in our dictionary then append it to recommendations
                 if sep_word in dictionary:
                     rec.add(sep_word)
+    #Return the set containing recommendations
     return rec
 
-
+# ------------------------------- Main function ------------------------------ #
 def main():
+    #Function to read the given dictionary.txt
     read_file()
+    #Make dictionary from the merged sentences
     dictionary = make_dictionary(sentences_merged)
+    #Insert main words in the Trie
     dictionary_trie = insert_main_words()
     
 main()
